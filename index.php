07ef7d315686ee6333c249d47783101e72088cbd
aahesam <45318539+aahesam@users.noreply.github.com>
2018-11-30 15:26:12 +0330

    Update index.php

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ $botids = $GetINFObot->result->username;
 define('API_KEY', $API_KEY);
 function bot($method, $datas = [])
 {
-    $url = "https://api.telegram.org/bot"$API_KEY"/"$method;
+    $url = "https://api.telegram.org/bot" . API_KEY . "/" . $method;
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
