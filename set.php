a694ce4b720531c57804866b5ec678aa632c162e
aahesam <45318539+aahesam@users.noreply.github.com>
2018-12-07 00:55:59 +0330

    Delete set.php

--- set.php
+++
@@ -1,7 +0,0 @@
-<?php
-$API_KEY = '637271484:AAEv2GBi-vMLKDSw_mxJGbZIMi1K8xbvkSQ';
-$hook_url = 'https://khabar-arz.ml/water/set.php';
-$hok="https://api.telegram.org/bot$API_KEY/setWebhook?url=$hook_url";
-echo file_get_contents($hok);
-
-?>
