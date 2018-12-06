c4d50f7137028efa5a99e4439707edf6f8490cf4
aahesam <45318539+aahesam@users.noreply.github.com>
2018-12-06 16:59:29 +0330

    Update set.php

--- set.php
+++ set.php
@@ -1,6 +1,6 @@
 <?php
-$API_KEY = '237966204:AAExLADwKL5KCi54dRF1nF7xLHH4dGCuewA';
-$hook_url = 'https://mrfenj.xyz/hoseinpor/bot/hoseinporbot.php';
+$API_KEY = '637271484:AAEv2GBi-vMLKDSw_mxJGbZIMi1K8xbvkSQ';
+$hook_url = 'https://khabar-arz.ml/water/set.php';
 $hok="https://api.telegram.org/bot$API_KEY/setWebhook?url=$hook_url";
 echo file_get_contents($hok);
 
