bec4a36c827f209fd8112c1d0a8c51ca52a08eac
aahesam <45318539+aahesam@users.noreply.github.com>
2018-12-09 20:48:17 +0000

    Delete ping.php

--- ping.php
+++
@@ -1,7 +0,0 @@
-<?php
-      if (in_array($userID, $admins)){
-
-	if($msg == '/ping'){
-              $MadelineProto->messages->sendMessage(['peer' => $chatID, 'reply_to_msg_id' => $msg_id ,'message' => '**Pong!**','parse_mode' => 'MarkDown']);
-            }
-      }
