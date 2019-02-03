ae0539a7f2a6a82f866c39582b40e8e5b5703ffe
aahesam <45318539+aahesam@users.noreply.github.com>
2019-02-04 02:32:51 +0330

    Update mybot.php

--- mybot.php
+++ mybot.php
@@ -40,24 +40,223 @@ $member_type = $telegram->getChatMember($content); // member Type
 
 if(!empty($text)){
 	
-	// Delete Bad Words
-	$bad_words = ['koon', 'kir', 'kos', 'kiri', 'kooni', 'kuni', 'کص', 'کیر', 'کیری', 'کونی', 'اوبی', 'obi','lashi', 'koskesh', 'kos kesh', 'لاشی', 'jende', 'جنده','کیون','kion','namal','نمال'];
-	$bwc = count($bad_words);
+	// monshi
+	$rie = ['#ریه'];
+	$rend = rand(000,999);
+	$bwc = count($rie);
 	for($i=0; $i<$bwc; $i++){
-		if(strstr(strtolower($text),$bad_words[$i])){
-			$content = ['chat_id' => $chat_id, 'message_id' => $message_id];
-			$telegram->deleteMessage($content);
+		if(strstr(strtolower($text),$rie[$i])){
+			$content = ['chat_id' => $chat_id, 'text'=>"نوبت [$rend] متخصص ریه", 'reply_to_message_id' => $message_id];
+			$telegram->sendmessage($content);
+			die(); // break code
+		}	
+	}
+	$ghalb = ['#قلب','#اکو'];
+	$rend = rand(000,999);
+	$bwc = count($ghalb);
+	for($i=0; $i<$bwc; $i++){
+		if(strstr(strtolower($text),$ghalb[$i])){
+			$content = ['chat_id' => $chat_id, 'text'=>"نوبت [$rend] متخصص قلب  ", 'reply_to_message_id' => $message_id];
+			$telegram->sendmessage($content);
+			die(); // break code
+		}	
+	}
+	$sono = ['#رادیولوژی','#سونو'];
+	$rend = rand(000,999);
+	$bwc = count($sono);
+	for($i=0; $i<$bwc; $i++){
+		if(strstr(strtolower($text),$sono[$i])){
+			$content = ['chat_id' => $chat_id, 'text'=>"نوبت [$rend] متخصص رادیولوژی", 'reply_to_message_id' => $message_id];
+			$telegram->sendmessage($content);
+			die(); // break code
+		}	
+	}
+	$mama = ['#زنان','#ماما'];
+	$rend = rand(000,999);
+	$bwc = count($mama);
+	for($i=0; $i<$bwc; $i++){
+		if(strstr(strtolower($text),$mama[$i])){
+			$content = ['chat_id' => $chat_id, 'text'=>"نوبت [$rend] متخصص زنان یا کارشناس مامایی", 'reply_to_message_id' => $message_id];
+			$telegram->sendmessage($content);
+			die(); // break code
+		}	
+	}
+	$dakheli = ['#داخلی'];
+	$rend = rand(000,999);
+	$bwc = count($dakheli);
+	for($i=0; $i<$bwc; $i++){
+		if(strstr(strtolower($text),$dakheli[$i])){
+			$content = ['chat_id' => $chat_id, 'text'=>"نوبت [$rend] متخصص داخلی", 'reply_to_message_id' => $message_id];
+			$telegram->sendmessage($content);
+			die(); // break code
+		}	
+	}
+	$ortoped = ['#ارتوپد'];
+	$rend = rand(000,999);
+	$bwc = count($ortoped);
+	for($i=0; $i<$bwc; $i++){
+		if(strstr(strtolower($text),$ortoped[$i])){
+			$content = ['chat_id' => $chat_id, 'text'=>"نوبت [$rend] متخصص ارتوپد", 'reply_to_message_id' => $message_id];
+			$telegram->sendmessage($content);
+			die(); // break code
+		}	
+	}
+	$post = ['#پوست'];
+	$rend = rand(000,999);
+	$bwc = count($post);
+	for($i=0; $i<$bwc; $i++){
+		if(strstr(strtolower($text),$post[$i])){
+			$content = ['chat_id' => $chat_id, 'text'=>"نوبت [$rend] متخصص پوست،مو و زیبایی", 'reply_to_message_id' => $message_id];
+			$telegram->sendmessage($content);
+			die(); // break code
+		}	
+	}
+	$eye = ['#چشم'];
+	$rend = rand(000,999);
+	$bwc = count($eye);
+	for($i=0; $i<$bwc; $i++){
+		if(strstr(strtolower($text),$eye[$i])){
+			$content = ['chat_id' => $chat_id, 'text'=>"نوبت [$rend] متخصص چشم", 'reply_to_message_id' => $message_id];
+			$telegram->sendmessage($content);
+			die(); // break code
+		}	
+	}
+	$ear = ['#گوش','#حلق','#بینی'];
+	$rend = rand(000,999);
+	$bwc = count($ear);
+	for($i=0; $i<$bwc; $i++){
+		if(strstr(strtolower($text),$ear[$i])){
+			$content = ['chat_id' => $chat_id, 'text'=>"نوبت [$rend] متخصص گوش،حلق و بینی", 'reply_to_message_id' => $message_id];
+			$telegram->sendmessage($content);
+			die(); // break code
+		}	
+	}
+	$ravanpe = ['#روانپزشک','#روانشناس'];
+	$rend = rand(000,999);
+	$bwc = count($ravanpe);
+	for($i=0; $i<$bwc; $i++){
+		if(strstr(strtolower($text),$ravanpe[$i])){
+			$content = ['chat_id' => $chat_id, 'text'=>"نوبت [$rend]  متخصص روانپزشکی یا کارشناس روانشناسی", 'reply_to_message_id' => $message_id];
+			$telegram->sendmessage($content);
+			die(); // break code
+		}	
+	}
+	$ofoni = ['#عفون'];
+	$rend = rand(000,999);
+	$bwc = count($ofoni);
+	for($i=0; $i<$bwc; $i++){
+		if(strstr(strtolower($text),$ofoni[$i])){
+			$content = ['chat_id' => $chat_id, 'text'=>"نوبت [$rend] متخصص عفونی", 'reply_to_message_id' => $message_id];
+			$telegram->sendmessage($content);
+			die(); // break code
+		}	
+	}
+	$asab = ['#مغز','#اعصاب'];
+	$rend = rand(000,999);
+	$bwc = count($asab);
+	for($i=0; $i<$bwc; $i++){
+		if(strstr(strtolower($text),$asab[$i])){
+			$content = ['chat_id' => $chat_id, 'text'=>"نوبت [$rend] متخصص مغز و اعصاب", 'reply_to_message_id'=>$message_id];
+			$telegram->sendmessage($content);
+			die(); // break code
+		}	
+	}
+	$orologh = ['#اورولوژ','#کلیه'];
+	$rend = rand(000,999);
+	$bwc = count($orologh);
+	for($i=0; $i<$bwc; $i++){
+		if(strstr(strtolower($text),$orologh[$i])){
+			$content = ['chat_id' => $chat_id, 'text'=>"نوبت [$rend] متخصص کلیه و‌مجاری ادراری", 'reply_to_message_id' => $message_id];
+			$telegram->sendmessage($content);
+			die(); // break code
+		}	
+	}
+	$azem = ['#آزمایش','#ازمایش'];
+	$rend = rand(000,999);
+	$bwc = count($azem);
+	for($i=0; $i<$bwc; $i++){
+		if(strstr(strtolower($text),$azem[$i])){
+			$content = ['chat_id' => $chat_id, 'text'=>"نوبت [$rend] دکتری یا کارشناس آزمایشگاه", 'reply_to_message_id' => $message_id];
+			$telegram->sendmessage($content);
+			die(); // break code
+		}	
+	}
+	$kodak = ['#کودک','#اطفال'];
+	$rend = rand(000,999);
+	$bwc = count($kodak);
+	for($i=0; $i<$bwc; $i++){
+		if(strstr(strtolower($text),$kodak[$i])){
+			$content = ['chat_id' => $chat_id, 'text'=>"نوبت [$rend] متخصص کودکان", 'reply_to_message_id' => $message_id];
+			$telegram->sendmessage($content);
+			die(); // break code
+		}	
+	}
+	$jarah = ['#جراح'];
+	$rend = rand(000,999);
+	$bwc = count($jarah);
+	for($i=0; $i<$bwc; $i++){
+		if(strstr(strtolower($text),$jarah[$i])){
+			$content = ['chat_id' => $chat_id, 'text'=>"نوبت [$rend] متخصص جراحی", 'reply_to_message_id' => $message_id];
+			$telegram->sendmessage($content);
+			die(); // break code
+		}	
+	}
+	$paras = ['#پرستار'];
+	$rend = rand(000,999);
+	$bwc = count($paras);
+	for($i=0; $i<$bwc; $i++){
+		if(strstr(strtolower($text),$paras[$i])){
+			$content = ['chat_id' => $chat_id, 'text'=>"نوبت [$rend] کارشناس پرستاری ", 'reply_to_message_id' => $message_id];
+			$telegram->sendmessage($content);
 			die(); // break code
 		}	
 	}
 	
-	// NAMAL
-//	if(strstr($text,'@Mohsen322Rezaei')){
-//		$content = ['chat_id' => $chat_id, 'text' => 'نمال...', 'reply_to_message_id' => $message_id];
-//		$telegram->sendMessage($content);
-//		die(); // break code
-//	}	
-//	
+	$dandan = ['#دندان'];
+	$rend = rand(000,999);
+	$bwc = count($dandan);
+	for($i=0; $i<$bwc; $i++){
+		if(strstr(strtolower($text),$dandan[$i])){
+			$content = ['chat_id' => $chat_id, 'text'=>"نوبت [$rend] دندانپزشک ", 'reply_to_message_id' => $message_id];
+			$telegram->sendmessage($content);
+			die(); // break code
+		}	
+	}
+	$omomi = ['#عمومی'];
+	$rend = rand(000,999);
+	$bwc = count($omomi);
+	for($i=0; $i<$bwc; $i++){
+		if(strstr(strtolower($text),$omomi[$i])){
+			$content = ['chat_id' => $chat_id, 'text'=>"نوبت [$rend] پزشک عمومی", 'reply_to_message_id' => $message_id];
+			$telegram->sendmessage($content);
+			die(); // break code
+		}	
+	}
+	$sex = ['#سکس'];
+	$rend = rand(000,999);
+	$bwc = count($sex);
+	for($i=0; $i<$bwc; $i++){
+		if(strstr(strtolower($text),$sex[$i])){
+			$content = ['chat_id' => $chat_id, 'text'=>"نوبت [$rend] متخصص سکس تراپی", 'reply_to_message_id' => $message_id];
+			$telegram->sendmessage($content);
+			die(); // break code
+		}	
+	}
+	$ghode = ['#غدد'];
+	$rend = rand(000,999);
+	$bwc = count($ghode);
+	for($i=0; $i<$bwc; $i++){
+		if(strstr(strtolower($text),$ghode[$i])){
+			$content = ['chat_id' => $chat_id, 'text'=>"نوبت [$rend] متخصص غدد", 'reply_to_message_id' => $message_id];
+			$telegram->sendmessage($content);
+			die(); // break code
+		}	
+	}
+		 if(strstr($text,'منشی')){
+	$content = ['chat_id' => $chat_id, 'text' => 'منشی سوپر گروه دکتر آنلاین هستم لطفا برای پرسش سوال خود و دریافت نوبت از من سوال خود را در قالب یک متن با شرح حال کامل با مشخص نمودن حوزه سوال خود با هشتگ (#) مشخص فرمایید به طور مثال #قلب', 'reply_to_message_id' => $message_id];
+	$telegram->sendMessage($content);
+	die();
+	}	
 }
 
 if(count($new_members) > 0){
